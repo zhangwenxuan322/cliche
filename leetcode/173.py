730451559728356461280b4d1f76fe966a979936
zhangwenxuan322 <798043150@qq.com>
2022-04-20 12:45:20 +1000

    20/04/2022

---
+++ leetcode/173.py
@@ -0,0 +1,33 @@
+# 173. Binary Search Tree Iterator
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class BSTIterator:
+
+    def __init__(self, root):
+        self.stack = list()
+        self.pushAll(root)
+
+    # @return a boolean, whether we have a next smallest number
+    def hasNext(self):
+        return self.stack
+
+    # @return an integer, the next smallest number
+    def next(self):
+        tmpNode = self.stack.pop()
+        self.pushAll(tmpNode.right)
+        return tmpNode.val
+
+    def pushAll(self, node):
+        while node is not None:
+            self.stack.append(node)
+            node = node.left
