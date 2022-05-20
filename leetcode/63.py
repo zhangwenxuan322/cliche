a954a7735c810d72d6bf73249635577152c28597
zhangwenxuan322 <798043150@qq.com>
2022-05-20 15:14:52 +1000

    20/05/2022

---
+++ leetcode/63.py
@@ -0,0 +1,19 @@
+# 63. Unique Paths II
+from typing import List
+
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        dp = [[0] * len(obstacleGrid[0]) for _ in range(len(obstacleGrid))]
+        dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+
+        for i in range(len(obstacleGrid)):
+            for j in range(len(obstacleGrid[0])):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    if i - 1 >= 0:
+                        dp[i][j] += dp[i - 1][j]
+                    if j - 1 >= 0:
+                        dp[i][j] += dp[i][j - 1]
+        return dp[-1][-1]
