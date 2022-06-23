e59badada05477964d720087cf905715b4f88e5a
zhangwenxuan322 <798043150@qq.com>
2022-06-23 15:28:05 +1000

    23/06/2022

---
+++ leetcode/630.py
@@ -0,0 +1,15 @@
+# 630. Course Schedule III
+import heapq
+from typing import List
+
+
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+        heap, time = [], 0
+        for t, end in sorted(courses, key=lambda x: x[1]):
+            time += t
+            heapq.heappush(heap, -t)
+            if time > end:
+                nt = heapq.heappop(heap)
+                time += nt
+        return len(heap)
