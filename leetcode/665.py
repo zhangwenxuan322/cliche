827dfb8dcf5d05f0c5b8da48546f2d7404bb2478
zhangwenxuan322 <798043150@qq.com>
2022-06-25 14:56:00 +1000

    25/06/2022

---
+++ leetcode/665.py
@@ -0,0 +1,13 @@
+# 665. Non-decreasing Array
+from typing import List
+
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        one, two = nums[:], nums[:]
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                one[i] = nums[i + 1]
+                two[i + 1] = nums[i]
+                break
+        return one == sorted(one) or two == sorted(two)
