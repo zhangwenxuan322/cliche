a00506aca0e81d899c7b629a98a8afd8abf78033
zhangwenxuan322 <798043150@qq.com>
2022-07-06 16:10:35 +1000

    06/07/2022

---
+++ leetcode/509.py
@@ -0,0 +1,8 @@
+# 509. Fibonacci Number
+from functools import cache
+
+
+class Solution:
+    @cache
+    def fib(self, n: int) -> int:
+        return n if n <= 1 else self.fib(n - 1) + self.fib(n - 2)
