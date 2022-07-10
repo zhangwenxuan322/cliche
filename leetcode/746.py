40cc95e354def2686e0fa4af60503d6f0d596358
zhangwenxuan322 <798043150@qq.com>
2022-07-10 13:33:13 +1000

    10/07/2022

---
+++ leetcode/746.py
@@ -0,0 +1,10 @@
+# 746. Min Cost Climbing Stairs
+
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        for i in range(2, len(cost)):
+            cost[i] += min(cost[i - 1], cost[i - 2])
+        return min(cost[-1], cost[-2])
