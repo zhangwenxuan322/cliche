b1c769cef4bdaeb72173ef594a7e81ebe55caff3
zhangwenxuan322 <798043150@qq.com>
2022-07-19 14:53:46 +1000

    19/07/2022

---
+++ leetcode/118.py
@@ -0,0 +1,12 @@
+# 118. Pascal's Triangle
+
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        pascal = [[1]*(i+1) for i in range(numRows)]
+        for i in range(numRows):
+            for j in range(1, i):
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
+        return pascal
