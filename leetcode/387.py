cba95f9a3f35df4872de6ca0aab0892ccf306495
zhangwenxuan322 <798043150@qq.com>
2022-08-16 11:15:50 +1000

    16/08/2022

---
+++ leetcode/387.py
@@ -0,0 +1,13 @@
+# 387. First Unique Character in a String
+
+from collections import Counter
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        counter = Counter(s)
+        for i, c in enumerate(s):
+            if counter[c] == 1:
+                return i
+
+        return -1
