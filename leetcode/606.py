4a02b99d42f826552f98ac2b5119e8bf50722060
zhangwenxuan322 <798043150@qq.com>
2022-09-07 13:40:09 +1000

    07/09/2022

---
+++ leetcode/606.py
@@ -0,0 +1,23 @@
+# 606. Construct String from Binary Tree
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        string = str(root.val)
+        if root.left:
+            string += "(" + self.tree2str(root.left) + ")"
+        if root.right:
+            if not root.left:
+                string += "()"
+            string += "(" + self.tree2str(root.right) + ")"
+        return string
