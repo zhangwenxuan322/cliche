bdf0fc9c0ecf1cdcab1b0913e83df42460fea5d1
zhangwenxuan322 <798043150@qq.com>
2022-09-08 15:59:57 +1000

    08/09/2022

---
+++ leetcode/94.py
@@ -0,0 +1,25 @@
+# 94. Binary Tree Inorder Traversal
+
+# Definition for a binary tree node.
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # recursively
+        res = []
+        self.helper(root, res)
+        return res
+
+    def helper(self, root, res):
+        if root:
+            self.helper(root.left, res)
+            res.append(root.val)
+            self.helper(root.right, res)
