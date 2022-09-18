b288013258d8a6d31a98e1553b470fe33cf5ce0b
zhangwenxuan322 <798043150@qq.com>
2022-09-18 21:26:34 +1000

    18/09/2022

---
+++ leetcode/42.py
@@ -0,0 +1,21 @@
+# 42. Trapping Rain Water
+
+from typing import List
+
+
+class Solution:
+    def trap(self, bars: List[int]) -> int:
+        if not bars or len(bars) < 3:
+            return 0
+        volume = 0
+        left, right = 0, len(bars) - 1
+        l_max, r_max = bars[left], bars[right]
+        while left < right:
+            l_max, r_max = max(bars[left], l_max), max(bars[right], r_max)
+            if l_max <= r_max:
+                volume += l_max - bars[left]
+                left += 1
+            else:
+                volume += r_max - bars[right]
+                right -= 1
+        return volume
