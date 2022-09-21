9586b463dd0141dca7f8c7d2ceeaaab97713fc21
zhangwenxuan322 <798043150@qq.com>
2022-09-21 14:44:23 +1000

    21/09/2022

---
+++ leetcode/985.py
@@ -0,0 +1,17 @@
+# 985. Sum of Even Numbers After Queries
+
+from typing import List
+
+
+class Solution:
+    def sumEvenAfterQueries(self, A: List[int], queries: List[List[int]]) -> List[int]:
+        s = sum(a for a in A if a % 2 == 0)
+        ans = []
+        for val, idx in queries:
+            if A[idx] % 2 == 0:
+                s -= A[idx]
+            A[idx] += val
+            if A[idx] % 2 == 0:
+                s += A[idx]
+            ans.append(s)
+        return ans
