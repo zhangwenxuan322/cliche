780515079f29ca571ea6688d0e9ccc910afcf1c9
zhangwenxuan322 <798043150@qq.com>
2022-09-23 12:48:56 +1000

    23/09/2022

---
+++ leetcode/1680.py
@@ -0,0 +1,8 @@
+# 1680. Concatenation of Consecutive Binary Numbers
+
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        ans, M = 0, 10**9 + 7
+        for x in range(n):
+            ans = (ans * (1 << (len(bin(x+1)) - 2)) + x+1) % M
+        return ans
