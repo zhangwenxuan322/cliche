12fb60a2846c0af4f48422273bc891b8397f934c
zhangwenxuan322 <798043150@qq.com>
2022-09-26 11:05:44 +1000

    26/09/2022

---
+++ leetcode/990.py
@@ -0,0 +1,35 @@
+# 990. Satisfiability of Equality Equations
+
+import collections
+from typing import List
+
+
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        graph = collections.defaultdict(set)
+        notEquals = []
+
+        def canMeet(u, target, visited):
+            if u == target:
+                return True
+            visited.add(u)
+            for v in graph[u]:
+                if v in visited:
+                    continue
+                if canMeet(v, target, visited):
+                    return True
+            return False
+
+        for eq in equations:
+            if eq[1:3] == '!=':
+                a, b = eq.split('!=')
+                notEquals.append((a, b))
+                continue
+            u, v = eq.split('==')
+            graph[u].add(v)
+            graph[v].add(u)
+
+        for u, v in notEquals:
+            if canMeet(u, v, set()):
+                return False
+        return True
